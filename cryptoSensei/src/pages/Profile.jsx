b35d2d2aba22300c4137dc86a3292c6e18b3fcd1
unw10181 <uchennanwilliams@gmail.com>
2026-02-16 20:46:03 -0500

    tailwind styling

--- cryptoSensei/src/pages/Profile.jsx
+++ cryptoSensei/src/pages/Profile.jsx
@@ -1,1 +1,9 @@
-const ${page} = () => { }
+const ${page} = () => {
+    return (
+    <div className="min-h-screen p-8">
+      <h1 className="text-4xl font-gaming text-white neon-text">
+        ${page} Page - Coming Soon
+      </h1>
+    </div>
+  );
+ }
