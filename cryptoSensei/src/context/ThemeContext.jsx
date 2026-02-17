eb7bc88a3eb84c402eb263e35dbb8753da40467a
unw10181 <uchennanwilliams@gmail.com>
2026-02-16 21:12:18 -0500

    themeContext switch method

--- cryptoSensei/src/context/ThemeContext.jsx
+++ cryptoSensei/src/context/ThemeContext.jsx
@@ -2,3 +2,11 @@ import { createContext, useContext, useState, useEffect } from "react";
 import { STORAGE_KEYS, THEMES } from "../utils/constants";
 
 const ThemeContext = createContext();
+
+export const useTheme = () => {
+  const context = useContext(ThemeContext);
+  if (!context) {
+    throw new Error("useTheme must be used within a ThemeProvider");
+  }
+  return context;
+};
