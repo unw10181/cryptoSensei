ff8d6d8e8460d3c5282a9492477be5fa081bf879
unw10181 <uchennanwilliams@gmail.com>
2026-02-16 21:57:53 -0500

    added AOS

--- cryptoSensei/src/main.jsx
+++ cryptoSensei/src/main.jsx
@@ -1,10 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import Aos from "aos";
 
-createRoot(document.getElementById('root')).render(
+// Initialize AOS animations
+AOS.init({
+  duration: 800,
+  easing: "ease-in-out",
+  once: false,
+  mirror: true,
+  offset: 100,
+});
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
   </StrictMode>,
-)
+);
