5c8125a92f44d03536189b5469a3f1f61c445d0f
unw10181 <uchennanwilliams@gmail.com>
2026-02-16 20:46:52 -0500

    portfolio placeholder

---
+++ cryptoSensei/src/pages/Portfolios.jsx
@@ -0,0 +1,11 @@
+const ${page} = () => {
+  return (
+    <div className="min-h-screen p-8">
+      <h1 className="text-4xl font-gaming text-white neon-text">
+        ${page} Page - Coming Soon
+      </h1>
+    </div>
+  );
+};
+
+export default ${page};
