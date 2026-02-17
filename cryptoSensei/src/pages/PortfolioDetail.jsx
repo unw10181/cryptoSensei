abe9766be65e1a16c731ba03e6d334d9f0181ec9
unw10181 <uchennanwilliams@gmail.com>
2026-02-16 20:48:38 -0500

    login components

--- cryptoSensei/src/pages/PortfolioDetail.jsx
+++ cryptoSensei/src/pages/PortfolioDetail.jsx
@@ -4,3 +4,5 @@ import { motion } from 'framer-motion';
 import { LogIn, Mail, Lock, Eye, EyeOff } from 'lucide-react';
 import { useAuth } from '../context/AuthContext';
 import Button from '../components/common/Button';
+
+const Login = () => { }
