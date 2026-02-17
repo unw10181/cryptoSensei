14c4741a507b5d1df9481989d28c37aa2d3a7845
unw10181 <uchennanwilliams@gmail.com>
2026-02-16 21:15:32 -0500

    imported methods/ hooks

---
+++ cryptoSensei/src/components/layout/Navbar.jsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import { motion, AnimatePresence } from "framer-motion";
+import {
+  LayoutDashboard,
+  Briefcase,
+  TrendingUp,
+  Award,
+  User,
+  LogOut,
+  Menu,
+  X,
+  Sun,
+  Moon,
+  Zap,
+} from "lucide-react";
+import { useAuth } from "../../context/AuthContext";
+import { useTheme } from "../../context/ThemeContext";
+import Button from "../common/Button";
+import RankBadge from "../common/RankBadge";
