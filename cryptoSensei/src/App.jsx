f5d920172fdb1934f6b544d33b0f47b7725c2cf3
unw10181 <uchennanwilliams@gmail.com>
2026-02-16 21:41:30 -0500

    added routes

--- cryptoSensei/src/App.jsx
+++ cryptoSensei/src/App.jsx
@@ -1,13 +1,27 @@
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import { AuthProvider, useAuth } from "./context/AuthContext";
+import { ThemeProvider } from "./context/ThemeContext";
+import LoadingScreen from "./components/common/LoadingScreen";
+import Navbar from "./components/layout/Navbar";
 
-import './App.css'
+// Pages (to be created)
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Dashboard from "./pages/Dashboard";
+import Portfolios from "./pages/Portfolios";
+import PortfolioDetail from "./pages/PortfolioDetail";
+import Crypto from "./pages/Crypto";
+import Achievements from "./pages/Achievements";
+import Profile from "./pages/Profile";
+import "./App.css";
 
 function App() {
-  
-
-  return (
-    <>
-    </>
-  )
+  return <></>;
 }
 
-export default App
+export default App;
