0412f54db6cdf5e83edd80b239b40bdb82fdabbd
unw10181 <uchennanwilliams@gmail.com>
2026-02-16 20:48:11 -0500

    import dependencies

---
+++ cryptoSensei/src/pages/PortfolioDetail.jsx
@@ -0,0 +1,6 @@
+import { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { motion } from 'framer-motion';
+import { LogIn, Mail, Lock, Eye, EyeOff } from 'lucide-react';
+import { useAuth } from '../context/AuthContext';
+import Button from '../components/common/Button';
